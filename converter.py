6c8d5d4795a7955484635f892c78e3ca4a256980
Minhaas <minhaas@Mohammeds-MacBook-Pro.local>
2020-08-08 00:25:05 +0530

    Finished converter

--- converter.py
+++ converter.py
@@ -12,9 +12,14 @@ def con():
     pounds = kg*2.20462
     ounces = kg*35.274
     t1.insert(END, grams)
+    t2.insert(END, pounds)
+    t3.insert(END, ounces)
+
+l1=Label(window, text="Kilogram")
+l1.grid(row=0,column=0)
 
 b1=Button(window, text="Convert", command=con, highlightbackground="black")
-b1.grid(row=0, column=1)
+b1.grid(row=0, column=1)    
 
 newVar = StringVar()
 e1=Entry(window, textvariable=newVar)
@@ -23,5 +28,10 @@ e1.grid(row=0, column=2)
 t1=Text(window, width=20, height=1 )
 t1.grid(row=1, column=1)
 
+t2=Text(window, width=20, height=1 )
+t2.grid(row=1, column=2)
+
+t3=Text(window, width=20, height=1 )
+t3.grid(row=1, column=3)
 
 window.mainloop()
